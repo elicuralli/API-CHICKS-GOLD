43d7770a1ef1e6a1542827261e3ac1a1c5fb0fc7
elibeth <elibethleonela@gmail.com>
2023-09-29 19:59:01 -0400

    adding functions for Z

--- src/model/functions.py
+++ src/model/functions.py
@@ -42,5 +42,37 @@ class Actions_Gallon():
             self.x_level += max_transfer
         else:
             self.y_level += max_transfer
-        
     
+    def is_z_reached(self,z_measure):
+
+        #fill, empty,transfer gallons, until z is reached or until it is not posible continue
+        
+        while self.x_level + self.y_level != z_measure:
+
+            #verify z is reached or not in every cicle
+            if self.x_level+self.y_level == z_measure:
+                break
+
+            #find gallon with less capacity to fill
+            if self.x_level < self.y_level:
+                source = 'x'
+                target = 'y'
+            else:
+                source = 'y'
+                target = 'x'
+            
+            #fill gallon with less capacity
+            self.fill(source)
+
+            #transfer water to the target gallon
+            self.transfer(source,target)
+
+            #empty source gallon
+            self.empty(source)
+
+            #verify z is reached or not in every cicle
+            if self.x_level+self.y_level == z_measure:
+                break
+        
+        #here return level water in every gallon
+        return self.x_level, self.y_level
--- src/routes/gallons.py
+++ src/routes/gallons.py
@@ -9,23 +9,33 @@ def ask_values():
 #the user enters the values for X,Y,Z and i process them as a json request to return a dictionary
     try:
 
-        x_level = request.json['x level']
-        y_level = request.json['y level']
-        z_measure = request.json['z measurement']
+        x_level_str = request.json['x level']
+        y_level_str = request.json['y level']
+        z_measure_str = request.json['z measurement']
 
-        if(z_measure > x_level or z_measure > y_level):
-            return jsonify({"error": " Z exceeds maximum capacity for X or Y"}),400
+        # check is values are integer with isdigit()
+        if not x_level_str.isdigit() or not y_level_str.isdigit() or not z_measure_str.isdigit():
+            return jsonify({"error": "values must be integers"}), 400
+
+        # convert string to integers
+        x_level = int(x_level_str)
+        y_level = int(y_level_str)
+        z_measure = int(z_measure_str)
+
+        gallon_manager = Actions_Gallon(x_level,y_level)
+
+        """if z_measure> x_level+y_level:
+            return jsonify({"error": "Z exceeds maximum capacity for X or Y"}), 400
+        """
         
-        else:
-            if x_level > y_level:
-                gallon = Actions_Gallon.fill(y_level)
-            else: 
-                gallon = Actions_Gallon.fill(x_level)
+        if z_measure> x_level or z_measure >y_level:
+            return jsonify({"error": "There's no solution Z exceeds maximum capacity for X or Y "}), 400
         
-        return jsonify({"ok":True,"status":200,"data":{"message":gallon}})
-    
+    # Call existing is_z_reached function to perform the remaining operations
+
+        x_level, y_level = gallon_manager.is_z_reached(z_measure)
+
+        return jsonify({"ok": True, "status": 200, "data": {"message": "Z has been reached", "x_level": x_level, "y_level": y_level}})
 
     except Exception as ex:
         return jsonify({"ok":False,"status": 500,"data":{"message": str(ex)}})
-
-#@gallon.route('/')
